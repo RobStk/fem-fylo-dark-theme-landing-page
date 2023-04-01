52a6232926757b92e44dd3400543b8fb27284765
robster <99953834+RobStk@users.noreply.github.com>
2023-04-01 14:42:19 +0200

    add TypeableInput class

---
+++ src/modules/TypeableInput.ts
@@ -0,0 +1,35 @@
+import IInputValidator from "./interfaces/IInputValidator";
+import ITypeableInput from "./interfaces/ITypeableInput";
+
+class TypeableInput implements ITypeableInput {
+	private containerElement: HTMLElement;
+	private inputValidator: IInputValidator;
+
+	constructor(inputContainer: HTMLElement, inputValidator: IInputValidator) {
+		this.containerElement = inputContainer;
+		this.inputValidator = inputValidator;
+	}
+
+	validate(): boolean {
+		const inputElement = this.containerElement.querySelector("input");
+		if (!inputElement) return true;
+		if (inputElement.required && !inputElement.value) return false;
+		if (!inputElement.required && !inputElement.value) return true;
+		const errors = this.inputValidator.validate(inputElement);
+		const isValid = !errors.length;
+		if (isValid) this.unsetErrorState(); else this.setErrorState(errors[0]);
+		return isValid;
+	}
+
+	private setErrorState(errorMsg: string) {
+		this.containerElement.classList.add("invalid");
+		this.containerElement.dataset.dataError = errorMsg;
+	}
+
+	private unsetErrorState() {
+		this.containerElement.classList.remove("invalid");
+		this.containerElement.dataset.dataError = "";
+	}
+}
+
+export default TypeableInput;
---
+++ src/modules/interfaces/ITypeableInput.ts
@@ -0,0 +1,5 @@
+interface ITypeableInput {
+	validate(): boolean;
+}
+
+export default ITypeableInput;
