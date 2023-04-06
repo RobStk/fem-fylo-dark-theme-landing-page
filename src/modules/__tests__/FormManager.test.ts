f489d5f06c69dc41de691325f22ba07d045b9915
robster <99953834+RobStk@users.noreply.github.com>
2023-04-06 14:57:37 +0200

    add form type check in FormManager constructor

--- src/modules/__tests__/FormManager.test.ts
+++ src/modules/__tests__/FormManager.test.ts
@@ -3,7 +3,7 @@
 */
 import { fireEvent } from "@testing-library/dom";
 import IInputValidator from "../interfaces/IInputValidator";
-import FormManager, { FAIL_MSG, SUCCESS_MSG } from "../FormManager";
+import FormManager, { FAIL_MSG, FORM_ERROR_MSG, SUCCESS_MSG } from "../FormManager";
 import ITypeableInputFactory from "../interfaces/ITypeableInputFactory";
 import ITypeableInput from "../interfaces/ITypeableInput";
 
@@ -37,6 +37,25 @@ console.log = jest.fn();
 
 describe("Form Manager", () => {
 	beforeEach(jest.clearAllMocks);
+
+	test("should throw an error if the form type IS NOT HTMLFormElement", () => {
+		const div = document.createElement("div");
+		div.classList.add("div");
+		document.body.appendChild(div);
+		const testForm1 = document.querySelector(".div") as HTMLFormElement;
+		const testForm2 = document.querySelector(".nonexistent") as HTMLFormElement;
+
+		expect(()=>new FormManager(testForm1, fakeInputsFactory, fakeInputValidator)).toThrowError(FORM_ERROR_MSG);
+		expect(()=>new FormManager(testForm2, fakeInputsFactory, fakeInputValidator)).toThrowError(FORM_ERROR_MSG);
+	});
+
+	test("should NOT throw an error if the form type IS HTMLFormElement", () => {
+		const div = document.createElement("form");
+		document.body.appendChild(div);
+		const testForm = document.querySelector("form") as HTMLFormElement;
+
+		expect(() => new FormManager(testForm, fakeInputsFactory, fakeInputValidator)).not.toThrowError();
+	});
 	
 	test("should prevent default on submit", () => {
 		const testForm = form.cloneNode(true) as HTMLFormElement;
